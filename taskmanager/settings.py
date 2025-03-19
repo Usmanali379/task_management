9b4065c6a2575a0518c1243ffa9a1fad8aa7d7b0
UsmanAli379 <ausmanali379@gmail.com>
2025-03-19 06:10:28 +0500

    Prepare for deployment

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -19,13 +19,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-14h82gsu#7^-!^%#_i#wo(un5rr_#3kg%$peju8d#^%#nxvw9i'
+import os
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'change-this-in-production')
+DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '*').split(',')
 
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -41,7 +40,10 @@ INSTALLED_APPS = [
 ]
 
 
+
+
 MIDDLEWARE = [
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
